d200bbf8eb840825b87c1cd5c9dc2eeda3451630
Jeffrey Serio <hyperreal@unixcat.coffee>
2022-02-22 20:36:20 -0600

    First commit

---
+++ github-fetch.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# From https://github.com/martinpitt/ostree-pitti-workstation/blob/pitti-desktop-f35/github-fetch.sh
+
+# Download built GitHub OSTree repository artifact and unpack it into a plain directory
+set -eux
+
+# download latest repo build
+REPO_FINAL="$(dirname $0)/fedora-xfice"
+REPO="${REPO_FINAL}.new"
+
+CURL="curl -u token:$(cat ~/.config/github-token) --show-error --fail"
+RESPONSE=$($CURL --silent https://api.github.com/repos/hyperreal64/ostree-fedora-xfice/actions/artifacts)
+ZIP=$(echo "$RESPONSE" | jq --raw-output '.artifacts | map(select(.name == "repository"))[0].archive_download_url')
+echo "INFO: Downloading $ZIP ..."
+[ -e /tmp/repository.zip ] || $CURL -L -o /tmp/repository.zip "$ZIP"
+rm -rf "$REPO"
+mkdir -p "$REPO"
+unzip -p /tmp/repository.zip | tar -xzC "$REPO"
+rm /tmp/repository.zip
+[ ! -e "$REPO_FINAL" ] || mv "${REPO_FINAL}" "${REPO_FINAL}.old"
+mv "$REPO" "$REPO_FINAL"
+rm -rf "${REPO_FINAL}.old"
